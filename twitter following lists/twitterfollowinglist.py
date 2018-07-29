3e620bed5457dedc3c2a86fe29c3a72563edc2e7
Vishal Kumar <vishukr785@gmail.com>
2018-07-29 16:57:38 +0530

    Update twitterfollowinglist.py

--- twitter following lists/twitterfollowinglist.py
+++ twitter following lists/twitterfollowinglist.py
@@ -4,10 +4,10 @@ import twitter
 # key and token to authentication
 '''notes: change key and token details'''
 
-consumer_key = 	'E0j9Emk2OAIjOJWIGxNtouo4s'
-consumer_secret = 'Z1j8ltnynmCAkjruTbDviouBtAN3YL6vZfcFQ7YNV0vsedN0fX'
-access_token = '810148484829609984-v2LoYkoIOvHgHsiFcY9hGF4EbQWwBQR'
-access_token_secret = '51c7KbrG6F6I3XiSLslAiA5Otn2OQeJGx0MQ6oPgugACi'
+consumer_key = 	'E**************s'
+consumer_secret = 'Z************fX'
+access_token = '8********************R'
+access_token_secret = '5*******************i'
 
 # api authentication process
 api = twitter.Api(consumer_key, consumer_secret, access_token, access_token_secret)
